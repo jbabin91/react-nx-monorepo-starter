a4ace8236c97fcd7be8f1d63de17f5ed22adfe71
Jace Babin <jbabin91@gmail.com>
2023-11-28 20:43:55 -0700

    init project
    Signed-off-by: Jace Babin <jbabin91@gmail.com>

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,34 +1,25 @@
 /// <reference types='vitest' />
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   cacheDir: '../../node_modules/.vite/web',
-
-  server: {
-    port: 4200,
-    host: 'localhost',
-  },
-
-  preview: {
-    port: 4300,
-    host: 'localhost',
-  },
-
   plugins: [react(), nxViteTsPaths()],
-
-  // Uncomment this if you are using workers.
-  // worker: {
-  //  plugins: [ nxViteTsPaths() ],
-  // },
-
+  preview: {
+    host: 'localhost',
+    port: 4300,
+  },
+  server: {
+    host: 'localhost',
+    port: 4200,
+  },
   test: {
-    globals: true,
     cache: {
       dir: '../../node_modules/.vitest',
     },
     environment: 'jsdom',
+    globals: true,
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
   },
 });
